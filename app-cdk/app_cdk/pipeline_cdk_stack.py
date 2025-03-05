b43d0ec594a38679847bf6a0f2359efb7b5b4269
hninpwintcho <hninpwintcho155@gmail.com>
2025-03-05 01:34:09 +0000

    Added CICD Dashboard and Build Success and Failure widget

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -10,6 +10,8 @@ from aws_cdk import (
     aws_iam as iam,
     aws_ssm as ssm,
     aws_codedeploy as codedeploy,
+    aws_cloudwatch as cloudwatch,
+    Duration as duration,
 )
 
 class PipelineCdkStack(Stack):
@@ -205,4 +207,35 @@ class PipelineCdkStack(Stack):
         CfnOutput(
             self, 'SourceConnectionStatus',
             value = SourceConnection.attr_connection_status
+        )
+
+        build_rate = cloudwatch.GraphWidget(
+          title="Build Successes and Failures",
+          width=6,
+          height=6,
+          view=cloudwatch.GraphWidgetView.PIE,
+          left=[
+              cloudwatch.Metric(
+                  namespace="AWS/CodeBuild",
+                  metric_name="SucceededBuilds",
+                  statistic='sum',
+                  label='Succeeded Builds',
+                  period=duration.days(30)
+              ),
+              cloudwatch.Metric(
+                  namespace="AWS/CodeBuild",
+                  metric_name="FailedBuilds",
+                  statistic='sum',
+                  label='Failed Builds',
+                  period=duration.days(30)
+              )
+          ]
+        )
+
+        dashboard = cloudwatch.Dashboard(
+          self, 'CICD_Dashboard',
+          dashboard_name='CICD_Dashboard',
+          widgets=[
+              [build_rate]
+          ]
         )
